f538697fd58dabce6229f67c6db592f21ce45175
JunHyeok <wsxqaz6126@gmail.com>
2025-11-15 19:03:15 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.01 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String phone_number) {
+        String m = "*".repeat(phone_number.length()-4);
+        String answer = phone_number.substring(phone_number.length()-4);
+        return m+answer;
+    }
+}
