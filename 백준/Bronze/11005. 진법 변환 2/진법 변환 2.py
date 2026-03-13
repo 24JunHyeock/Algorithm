3bf6468517e0edf0bc8b01da16eff1656e787597
JunHyeok <wsxqaz6126@gmail.com>
2026-03-13 19:40:44 +0900

    [Bronze I] Title: 진법 변환 2, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -3,11 +3,11 @@ import sys
 input = sys.stdin.readline  #속도위해 필수
 
 R = []
-R2 = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+R2 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 M = list(map(int, input().split()))
 while(M[0]>0):
     R.append(R2[M[0]%M[1]])
     M[0]//=M[1]
 
-R.reverse()
-print(''.join(R))
+
+print(''.join(R[::-1]))
