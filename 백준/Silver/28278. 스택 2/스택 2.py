5433a87b9cb7a7b84d0be56e6711d2c07b22516d
JunHyeok <wsxqaz6126@gmail.com>
2026-04-07 19:01:51 +0900

    [Silver IV] Title: 스택 2, Time: 564 ms, Memory: 141380 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -1,34 +1,26 @@
 import sys
 
-def solve():
-    # 1. 모든 입력을 한 번에 읽어와서 속도 극대화
-    input_data = sys.stdin.read().split()
-    if not input_data:
-        return
-    
-    # 2. 반복문 최적화 (N을 별도로 관리)
-    it = iter(input_data)
-    N = int(next(it))
-    
-    stack = []
-    # 3. 출력 데이터를 리스트에 담아 한 번에 출력 (sys.stdout.write)
-    results = []
-    
-    for _ in range(N):
-        op = next(it)
-        
-        if op == "1":
-            stack.append(next(it))
-        elif op == "2":
-            results.append(stack.pop() if stack else "-1")
-        elif op == "3":
-            results.append(str(len(stack)))
-        elif op == "4":
-            results.append("0" if stack else "1")
-        elif op == "5":
-            results.append(stack[-1] if stack else "-1")
-            
-    sys.stdout.write("\n".join(results) + "\n")
+data = sys.stdin.read().split()
 
-if __name__ == "__main__":
-    solve()
+n = int(data[0])
+stack = []
+out = []
+
+i = 1
+for _ in range(n):
+    c = data[i]
+    i += 1
+    
+    if c == '1':
+        stack.append(int(data[i]))
+        i += 1
+    elif c == '2':
+        out.append(str(stack.pop()) if stack else '-1')
+    elif c == '3':
+        out.append(str(len(stack)))
+    elif c == '4':
+        out.append('0' if stack else '1')
+    else:
+        out.append(str(stack[-1]) if stack else '-1')
+
+sys.stdout.write('\n'.join(out) + '\n')
