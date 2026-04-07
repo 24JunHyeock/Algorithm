e78ebfba4004f3c780ccba24ddd8361db3d589b2
JunHyeok <wsxqaz6126@gmail.com>
2026-04-07 18:59:11 +0900

    [Silver IV] Title: 스택 2, Time: 252 ms, Memory: 117344 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -1,20 +1,34 @@
 import sys
 
-#sys.stdin = open('./input.txt', 'r')  # 제출시 주석
-input = sys.stdin.readline  # 속도위해 필수
+def solve():
+    # 1. 모든 입력을 한 번에 읽어와서 속도 극대화
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+    
+    # 2. 반복문 최적화 (N을 별도로 관리)
+    it = iter(input_data)
+    N = int(next(it))
+    
+    stack = []
+    # 3. 출력 데이터를 리스트에 담아 한 번에 출력 (sys.stdout.write)
+    results = []
+    
+    for _ in range(N):
+        op = next(it)
+        
+        if op == "1":
+            stack.append(next(it))
+        elif op == "2":
+            results.append(stack.pop() if stack else "-1")
+        elif op == "3":
+            results.append(str(len(stack)))
+        elif op == "4":
+            results.append("0" if stack else "1")
+        elif op == "5":
+            results.append(stack[-1] if stack else "-1")
+            
+    sys.stdout.write("\n".join(results) + "\n")
 
-N = int(input())
-stack = []
-
-for _ in range(N):
-    str = input().split()
-    if str[0]=="1":
-        stack.append(str[1])
-    elif str[0]=="2":
-        print(stack.pop() if stack else -1)
-    elif str[0]=="3":
-        print(len(stack))
-    elif str[0]=="4":
-        print(0 if stack else 1)
-    elif str[0]=="5":
-        print(stack[-1] if stack else -1)
+if __name__ == "__main__":
+    solve()
