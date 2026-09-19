7446f9b2ead946764e21c7eacc291f1e2f7e1ad9
JunHyeok <wsxqaz6126@gmail.com>
2026-09-19 18:45:16 +0900

    [level 2] Title: 피보나치 수, Time: 0.33 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int a = 0; 
+    int b = 1; 
+    int c = 0;
+    for (int i = 2; i <= n; i++) {
+        c = (a + b) % 1234567; 
+        a = b;
+        b = c;
+    }
+
+    return c;
+}
